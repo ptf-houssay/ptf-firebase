3a75e7b7bd180052c4e45c10f5f5b490ecb5c90a
JuanIgnacioCuiule <juanignaciocuiule@gmail.com>
2016-09-26 22:40:57 -0300

    separación config

---
+++ js/config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDhlJa4l5_nPrQ1WGsXRHMjKmrnruxZ7jc",
+	authDomain: "stock-9fa62.firebaseapp.com",
+	databaseURL: "https://stock-9fa62.firebaseio.com/",
+	storageBucket: "stock-9fa62.appspot.com",
+	messagingSenderId: "675707266004"
+};
+firebase.initializeApp(config);
